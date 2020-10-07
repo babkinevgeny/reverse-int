0986f1c79194816486239b6228d3dad9a49c1054
babkinevgeny <babkinevgeny@gmail.com>
2020-10-07 12:51:35 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    const arr = n.toString().split('');
+
+    if ( n < 0) {
+        arr.shift();
+    }
+
+    const reversedNumber = +arr.reverse().join('');
+
+    return reversedNumber;
 }
